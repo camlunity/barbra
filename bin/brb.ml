b0a50aa21e636d8c1b3044d512899558ac5cd607
Roman Sokolov <sokolov.r.v@gmail.com>
2011-12-11 19:15:53 +0400

    вынес описание типов в отдельные файл cont.

--- bin/brb.ml
+++ bin/brb.ml
@@ -1,12 +1,13 @@
 open Config
+open Types
 open Printf
 
-let config = Config.parse_config "config" 
+let config = Config.parse_config "config"
 
 let () = List.iter (fun (name,wher) -> match wher with
   | VCS (url,typ) -> printf "%s: VCS.%s %s\n" name (string_of_vcs_type typ) url
   | FsSrc src -> printf "%s: FsSrc %s\n" name src
   | Local -> printf "%s: Local\n" name
-  | HttpArchive (s,t) -> printf "%s: HttpArchive %s\n" name s 
+  | HttpArchive (s,t) -> printf "%s: HttpArchive %s\n" name s
   | FsArchive (s,t) -> printf "%s: FsArchive %s\n" name s
 ) config
--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,7 +1,7 @@
 (* OASIS_START *)
-(* DO NOT EDIT (digest: e747b87e96a8d571620b20b9374da0ce) *)
+(* DO NOT EDIT (digest: f065fcea8a396dfeeda793acf6c580b3) *)
 module OASISGettext = struct
-# 21 "/mnt/macos_vb/installed/oasis-0.2.1~alpha1/src/oasis/OASISGettext.ml"
+# 21 "/home/typhon/kamlo/oasis-0.2.1~alpha1/src/oasis/OASISGettext.ml"
   
   let ns_ str =
     str
@@ -24,7 +24,7 @@ module OASISGettext = struct
 end
 
 module OASISExpr = struct
-# 21 "/mnt/macos_vb/installed/oasis-0.2.1~alpha1/src/oasis/OASISExpr.ml"
+# 21 "/home/typhon/kamlo/oasis-0.2.1~alpha1/src/oasis/OASISExpr.ml"
   
   
   
@@ -115,7 +115,7 @@ end
 
 
 module BaseEnvLight = struct
-# 21 "/mnt/macos_vb/installed/oasis-0.2.1~alpha1/src/base/BaseEnvLight.ml"
+# 21 "/home/typhon/kamlo/oasis-0.2.1~alpha1/src/base/BaseEnvLight.ml"
   
   module MapString = Map.Make(String)
   
@@ -212,7 +212,7 @@ end
 
 
 module MyOCamlbuildFindlib = struct
-# 21 "/mnt/macos_vb/installed/oasis-0.2.1~alpha1/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
+# 21 "/home/typhon/kamlo/oasis-0.2.1~alpha1/src/plugins/ocamlbuild/MyOCamlbuildFindlib.ml"
   
   (** OCamlbuild extension, copied from 
     * http://brion.inria.fr/gallium/index.php/Using_ocamlfind_with_ocamlbuild
@@ -321,7 +321,7 @@ module MyOCamlbuildFindlib = struct
 end
 
 module MyOCamlbuildBase = struct
-# 21 "/mnt/macos_vb/installed/oasis-0.2.1~alpha1/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 21 "/home/typhon/kamlo/oasis-0.2.1~alpha1/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
   
   (** Base functions for writing myocamlbuild.ml
       @author Sylvain Le Gall
@@ -336,7 +336,7 @@ module MyOCamlbuildBase = struct
   type name = string 
   type tag = string 
   
-# 55 "/mnt/macos_vb/installed/oasis-0.2.1~alpha1/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
+# 55 "/home/typhon/kamlo/oasis-0.2.1~alpha1/src/plugins/ocamlbuild/MyOCamlbuildBase.ml"
   
   type t =
       {
