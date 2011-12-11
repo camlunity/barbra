4706d369317a0398af1d9c7ce4e862b37f391c90
Sergei Lebedev <superbobry@gmail.com>
2011-12-11 20:29:34 +0400

    Added basic 'Arg' handling

--- bin/brb.ml
+++ bin/brb.ml
@@ -1,13 +1,25 @@
 open Config
-open Types
 open Printf
+open Types
 
-let config = Config.parse_config "config"
+let show_version = ref false
+let opts = Arg.align [
+  ("--version",
+   Arg.Set show_version,
+   "output version information and exit")
+]
 
-let () = List.iter (fun (name,wher) -> match wher with
-  | VCS (url,typ) -> printf "%s: VCS.%s %s\n" name (string_of_vcs_type typ) url
-  | FsSrc src -> printf "%s: FsSrc %s\n" name src
-  | Local -> printf "%s: Local\n" name
-  | HttpArchive (s,_t) -> printf "%s: HttpArchive %s\n" name s
-  | FsArchive (s,_t) -> printf "%s: FsArchive %s\n" name s
-) config
+let () = begin
+  Arg.parse opts ignore "brb";
+  if !show_version then
+    printf "%i\n" Barbra.version;
+
+  let config = parse_config "config" in
+  List.iter (fun (name, kind) -> match kind with
+    | VCS (url,typ) -> printf "%s: VCS.%s %s\n" name (string_of_vcs_type typ) url
+    | FsSrc src -> printf "%s: FsSrc %s\n" name src
+    | Local -> printf "%s: Local\n" name
+    | HttpArchive (s,_t) -> printf "%s: HttpArchive %s\n" name s
+    | FsArchive (s,_t) -> printf "%s: FsArchive %s\n" name s
+  ) config
+end
