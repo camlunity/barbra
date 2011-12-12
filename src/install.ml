db78063c59bd3ee107e025c642cf32b701ca52e0
Dmitry Grebeniuk <gdsfh1+umodni@gmail.com>
2011-12-12 18:57:30 +0200

    wtf

--- src/install.ml
+++ src/install.ml
@@ -1,6 +1,23 @@
 open Types
 
 
+(* variable that was not set will be restored to "". *)
+let with_env =
+  let open WithM in
+  let open Res in
+  let open WithRes in
+    { cons = fun (env_name, new_val) ->
+        let old_val =
+          try Unix.getenv env_name with Not_found -> ""
+        in
+        let () = Unix.putenv env_name new_val in
+        return (env_name, old_val)
+    ; fin = fun (env_name, old_val) ->
+        let () = Unix.putenv env_name old_val in
+        return ()
+    }
+
+
 let makefile : install_type = object
   method install ~source_dir =
     let open WithM in
