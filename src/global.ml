affcf9dc4f86534e34c0f0787c48d7d874a7520e
Dmitry Grebeniuk <gdsfh1+umodni@gmail.com>
2011-12-12 22:41:48 +0200

    .

--- src/global.ml
+++ src/global.ml
@@ -5,3 +5,5 @@ let brb_conf = "brb.conf"  (* просто имя файла без путей *
 let base_dir = "_dep"
 let src_dir = Filename.concat base_dir "src"
 let tmp_dir = Filename.concat base_dir "tmp"
+let bin_dir = Filename.concat base_dir "bin"
+let lib_dir = Filename.concat base_dir "lib"
