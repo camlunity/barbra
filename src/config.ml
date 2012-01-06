a13936680668bdadd0b9f5cda4534545770d17f4
Sergei Lebedev <superbobry@gmail.com>
2012-01-07 00:03:05 +0400

    Getting ready for patches and custom make targets

--- src/config.ml
+++ src/config.ml
@@ -4,8 +4,7 @@ open Types
 
 module List = ListLabels
 
-
-let parse_line_opt l =
+module Keywords = struct
   let guess_archive s ~succ ~fail =
     let (_, ext) = String.split s "." in
     match ext with
@@ -13,53 +12,55 @@ let parse_line_opt l =
       | "tar.bz2" -> succ `TarBzip2
       | "tar"     -> succ `Tar
       | _         -> fail ()
-  in
 
-  if l = "" then
-    None
-  else
-    some & Scanf.sscanf l " dep %s %s %s " (fun name typ src ->
-      let package = match String.lowercase typ with
-        | "remote" ->
-          guess_archive src
-            ~succ:(fun x -> Remote (x,src))
-            ~fail:(fun () -> Log.error "can't guess remote archive format: %S\n" typ)
-        | "remote-tar-gz"  -> Remote (`TarGz, src)
-        | "remote-tar-bz2" -> Remote (`TarBzip2, src)
-        | "remote-tar"     -> Remote (`Tar, src)
-        | "local" ->
-          guess_archive src
-            ~succ:(fun x -> Local (x, src))
-            ~fail:(fun () -> Log.error "can't guess local archive format: %S\n" typ)
-        | "local-tar-gz" -> Local (`TarGz, src)
-        | "local-tar-bz2" -> Local (`TarBzip2, src)
-        | "local-tar" -> Local (`Tar, src)
-        | "local-dir" -> Local (`Directory, src)
-        | "bundled-dir" -> Bundled (`Directory, src)
-        | "bundled" ->
-          guess_archive src
-            ~succ:(fun x -> Bundled (x,src))
-            ~fail:(fun () -> Log.error "can't guess bundle's archive format: %S\n" typ)
-        | "bundled-tar" -> Bundled (`Tar, src)
-        | "bundled-tar-gz" -> Bundled (`TarGz, src)
-        | "bundled-tar-bz2" -> Bundled (`TarBzip2, src)
-        | "svn" | "csv" | "hg" | "git" | "bzr" | "darcs" ->
-          VCS (vcs_type_of_string typ, src)
-        | _ -> Log.error "unsupported package type: %S when source = %S\n" typ src
-      in (name, package)
-    )
+  let dep args = Scanf.sscanf args " %s %s %s " (fun name typ src ->
+    let package = match String.lowercase typ with
+      | "remote" ->
+        guess_archive src
+          ~succ:(fun x -> Remote (x,src))
+          ~fail:(fun () -> Log.error "can't guess remote archive format: %S\n" typ)
+      | "remote-tar-gz"  -> Remote (`TarGz, src)
+      | "remote-tar-bz2" -> Remote (`TarBzip2, src)
+      | "remote-tar"     -> Remote (`Tar, src)
+      | "local" ->
+        guess_archive src
+          ~succ:(fun x -> Local (x, src))
+          ~fail:(fun () -> Log.error "can't guess local archive format: %S\n" typ)
+      | "local-tar-gz" -> Local (`TarGz, src)
+      | "local-tar-bz2" -> Local (`TarBzip2, src)
+      | "local-tar" -> Local (`Tar, src)
+      | "local-dir" -> Local (`Directory, src)
+      | "bundled-dir" -> Bundled (`Directory, src)
+      | "bundled" ->
+        guess_archive src
+          ~succ:(fun x -> Bundled (x,src))
+          ~fail:(fun () -> Log.error "can't guess bundle's archive format: %S\n" typ)
+      | "bundled-tar" -> Bundled (`Tar, src)
+      | "bundled-tar-gz" -> Bundled (`TarGz, src)
+      | "bundled-tar-bz2" -> Bundled (`TarBzip2, src)
+      | "svn" | "csv" | "hg" | "git" | "bzr" | "darcs" ->
+        VCS (vcs_type_of_string typ, src)
+      | _ -> Log.error "unsupported package type: %S when source = %S\n" typ src
+    in (name, package)
+  )
+end
+
+let parse_line_opt = function
+  | ""   -> None
+  | line ->
+    let (keyword, args) = String.split line " " in
+    some & match keyword with
+      | "dep" -> Keywords.dep args
+      | _     -> Log.error "Invalid keyword: %S" keyword
 
 let filter_comments =
-  let line_means_something line =
-    let rec inner = function
-      | 0 -> false
-      | i -> begin match line.[i] with
-          | '#' -> false
-          (* FIXME(superbobry): why those characters? *)
-          | '\x20' | '\x09' | '\x0A' | '\x0D' -> inner (i + 1)
-          | _ -> true
-      end
-    in inner (String.length line - 1)
+  let line_means_something =
+    String.fold_left (fun acc ch -> match ch with
+      (* FIXME(superbobry): why those characters? *)
+      | '\x20' | '\x09' | '\x0A' | '\x0D' -> acc
+      | '#' -> acc && false
+      | _   -> acc && true
+    ) true
   in
 
   Stream.map_filter (fun line ->
